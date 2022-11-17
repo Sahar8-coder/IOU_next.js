9ecb1def1cee54f32de158a140255de777a65d7a
Sahar8-coder <ze.kirilov@gmail.com>
2022-11-17 20:36:30 +0200

    .

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -3,17 +3,21 @@ export default async function (req, res) {
     require('dotenv').config()
 
     let transporter = nodemailer.createTransport({
-      host: "smtp.yandex.ru",
+      host: "smtp.mail.ru",
       port: 465,
       secure: true, // true for 465, false for other ports
+      /*auth: {
+        user: 'k1rilovz@yandex.ru',
+        pass: 'qhfxkhjcdlvgdwhs',
+      },*/
       auth: {
-        user: 'k1rilovz@yandex.ru', // generated ethereal user
-        pass: 'qhfxkhjcdlvgdwhs', // generated ethereal password
+        user: 'quadratic.eq@mail.ru',
+        pass: 'kEm4eZ7HRbFVAyupfcH5',
       },
     });
-    console.log(transporter)
+    //console.log(transporter)
     const mailData = {
-        from: 'k1rilovz@yandex.ru',
+        from: 'quadratic.eq@mail.ru',
         to: req.body.email,
         subject: `Message From ${req.body.name}`,
         text: req.body.message + " | Sent from: " + req.body.email,
